f8420881ddca5692195ecbc5da10e0c9d02eb43f
Chocla <chocla1@cock.li>
2017-12-04 22:55:37 -0800

    Day 5 Part 2

--- day5/main.go
+++ day5/main.go
@@ -17,13 +17,18 @@ func main(){
 		path = append(path,num)
 	}
 	fmt.Println(followPath(path))
-	
+	// a := []int{0,3,0,1,-3}
+	// fmt.Println(followPath(a))
 }
 
 func followPath(path []int)(steps int) {
 	for i := 0; i >= 0 && i < len(path); {	
-		path[i]++
-		i += path[i]-1
+		if path[i] > 3 {
+			path[i]--
+		} else {
+			path[i]++
+		}
+		i += path[i] - 1
 		steps++
 	}
 	return
