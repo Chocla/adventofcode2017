82cb3c72e2bbf63140999d9f406be3d6e84caf7a
Chocla <chocla1@cock.li>
2017-12-05 22:56:33 -0800

    fixed day 6

--- day6/main.go
+++ day6/main.go
@@ -6,14 +6,10 @@ import (
 
 func main(){
 	//input := []int{0,2,7,0}
-	input := []int{5,1,10,0,1,7,13,14,3,12,8,10,7,12,0,6}
-	var seenArrays [][]int
-	seenArrays = append(seenArrays,[]int{5,1,10,0,1,7,13,14,3,12,8,10,7,12,0,6})
-	//seenArrays = append(seenArrays, []int{0,2,7,0})
+	input := []int{5,1,10,0,1,7,13,14,3,12,8,10,7,12,0,6}	
 	seen := false
 	count := 0
 	m := make(map[int][]int)
-
 	for !seen {
 		count++
 		max,index := max(input)
@@ -21,14 +17,13 @@ func main(){
 			index = (index + 1) % len(input)
 			input[index]++
 		}
-		for i := range seenArrays {
-			if equal(input,seenArrays[i]) {
+		for i := range m {
+			if equal(input,m[i]) {
 				seen = true
 			}
 		}
 		a := make([]int, len(input))
 		copy(a,input)
-		seenArrays = append(seenArrays,a)
 		m[count] = a
 	}
 	for i := range m {
