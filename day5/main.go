9833b33a89ef9b4a7145b9a42e7ef07a9811ec9a
Chocla <chocla1@cock.li>
2017-12-06 22:29:07 -0800

    Day 7 Part 1

--- day5/main.go
+++ day5/main.go
@@ -8,6 +8,7 @@ import (
 )
 
 func main(){
+	//get path
 	f,_ := os.Open("path.txt")
 	scanner := bufio.NewScanner(f)
 	path := make([]int,0)
@@ -16,18 +17,21 @@ func main(){
 		num,_ := strconv.Atoi(tmp)
 		path = append(path,num)
 	}
+	
 	fmt.Println(followPath(path))
 	// a := []int{0,3,0,1,-3}
 	// fmt.Println(followPath(a))
 }
 
 func followPath(path []int)(steps int) {
-	for i := 0; i >= 0 && i < len(path); {	
+	for i := 0;i < len(path); {	
+		//updates offset
 		if path[i] > 3 {
 			path[i]--
 		} else {
 			path[i]++
 		}
+		//moves
 		i += path[i] - 1
 		steps++
 	}
--- day6/main.go
+++ day6/main.go
@@ -8,28 +8,27 @@ func main(){
 	//input := []int{0,2,7,0}
 	input := []int{5,1,10,0,1,7,13,14,3,12,8,10,7,12,0,6}	
 	seen := false
-	count := 0
+	count := 0 //cycles
 	m := make(map[int][]int)
+
 	for !seen {
-		count++
+		count++ 
 		max,index := max(input)
+		
 		for input[index] = 0; max > 0; max--{
 			index = (index + 1) % len(input)
 			input[index]++
 		}
+
 		for i := range m {
 			if equal(input,m[i]) {
+				fmt.Println(count-i)
 				seen = true
 			}
 		}
 		a := make([]int, len(input))
 		copy(a,input)
 		m[count] = a
-	}
-	for i := range m {
-		if equal(m[i],input) && i != count{
-			fmt.Println(count-i)
-		}
 	}
 }
 func equal(a,b []int)(bool) {
