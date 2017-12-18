27c7133049d32969273f63e67ed2862288cd3d75
Chocla <chocla1@cock.li>
2017-12-18 14:42:43 -0800

    Day 15 - Part 1

---
+++ day15/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+)
+const (
+	iterations = 40000000
+	factorA = 16807
+	factorB = 48271
+	modValue = 2147483647 //2^32 - 1
+	comparisonMod = 65336 //2^16
+)
+
+func simulation(a,b int) (equal int) {
+	for i := 0; i < iterations; i++ {
+		a,b = genVal(a,factorA),genVal(b,factorB)
+		if compareValues(a,b) {
+			equal++
+		}
+	}
+
+	return
+}
+
+func genVal(prevVal, factor int) (val int) {
+	return (prevVal*factor) % modValue
+}
+
+func compareValues(valA, valB int) (bool) {
+	return (valA % 65536) == (valB % 65536)
+}
+
+func main() {
+	fmt.Println(simulation(277,349))
+}
