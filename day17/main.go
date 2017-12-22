863212430fe4e50f9a497701f56847192e1b0e98
Chocla <chocla1@cock.li>
2017-12-22 14:29:07 -0800

    Day 17 Part 2

--- day17/main.go
+++ day17/main.go
@@ -8,9 +8,10 @@ const (
 	testStepSize = 3
 	inputStepSize = 349
 	testIter = 10
-	inputIter = 2018
+	inputIter = 50000001
 )
 
+//Part 1 Answer - Value after 2017
 func findAnswer(lock []int) (int) {
 	for i := range lock {
 		if lock[i] == 2017 {
@@ -19,19 +20,24 @@ func findAnswer(lock []int) (int) {
 	}
 	return 0
 }
-func spinLock(step,iter int) (lock []int) {
-	position := 0
-	lock = append(lock,0)
 
-	for i := 1; i < iter; i++ {
-		position = (position + step) % len(lock) + 1
-		lock = updateLock(position,i,lock)
-		//fmt.Println(lock,position)
+//builds the spinlock with a given step and size
+func spinLock(step,size int) ([]int) {
+	position := 0
+	lock := make([]int, 2)
+
+	for i := 1; i < size; i++ {
+		position = (position + step) % i + 1
+		if position == 1 {
+			lock[1] = i
+		}
 	}
 
-	return
+	return lock
 }
 
+
+//insert value into lock
 func updateLock(pos, i int, lock []int) ([]int) {
 	tmp := make([]int, len(lock) + 1)
 	for j := 0; j < pos; j++ {
@@ -41,11 +47,10 @@ func updateLock(pos, i int, lock []int) ([]int) {
 	for j := pos + 1; j < len(tmp);j++ {
 		tmp[j] = lock[j-1]
 	}
-
 	return tmp
 }
 func main() {
 	t0 := time.Now()
-	answer := findAnswer(spinLock(inputStepSize,inputIter))
-	fmt.Println("Answer: ", answer, ". Time: ",time.Since(t0))
+	lock := spinLock(inputStepSize,inputIter)
+	fmt.Println("Answer: ", lock[1], "\nTime: ",time.Since(t0))
 }
