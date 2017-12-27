9ff6825b4e9b0d6e578ce167dcb5facdc3d923e5
Chocla <chocla1@cock.li>
2017-12-26 19:32:28 -0800

    Day 20 Part 2

--- day20/main.go
+++ day20/main.go
@@ -6,7 +6,6 @@ import (
 	"bufio"
 	"strings"
 	"strconv"
-	"math"
 )
 
 type vector struct {
@@ -19,6 +18,7 @@ type particle struct {
 	pos vector
 	vel vector
 	acc vector
+	col bool
 }
 
 func step(p *particle) {
@@ -29,41 +29,67 @@ func step(p *particle) {
 	p.pos.y += p.vel.y
 	p.pos.z += p.vel.z
 }
-
-func parseAccel() {
-	min := 11.0
+func parseVector(input string) (v vector) {
+	input = strings.TrimRight(strings.TrimLeft(input,"<"),">")
+	split := strings.Split(input,",")
+	v.x,_ = strconv.Atoi(split[0])
+	v.y,_ = strconv.Atoi(split[1])
+	v.z,_ = strconv.Atoi(split[2])
+	return
+}
+func parseInput() (particles []particle) {
 	file,_ := os.Open("input.txt")
 	scanner := bufio.NewScanner(file)
-	index := 0
-	for i := 0;scanner.Scan(); i++{
+	
+	for scanner.Scan(){
 		line := scanner.Text()
-		split := strings.Split(line, "a=")
-		split[1] = strings.TrimRight( strings.TrimLeft(split[1],"<"),">")
-		partAccel := strings.Split(split[1],",")
-		x,_ := strconv.Atoi(partAccel[0])
-		y,_ := strconv.Atoi(partAccel[1])
-		z,_ := strconv.Atoi(partAccel[2])
-		a := math.Abs(float64(x)) + math.Abs(float64(y)) + math.Abs(float64(z))
-
-		if a <= min {
-			fmt.Println(line)
-			min = a
-			index = i
-		}
-
+		split := strings.Split(line, ", ")
+		pos,vel,acc := parseVector(split[0][2:]),parseVector(split[1][2:]),parseVector(split[2][2:])
+		particles = append(particles, particle{pos,vel,acc,false})
+		
 	}
-	fmt.Println(index)
-}
-func distance(p particle) (int) {
-	return p.pos.x + p.pos.y + p.pos.z
-}
-func simulateParticles() {
-
+	return
 }
 
-//in the long run, the particle with the lowest acceleration will stay closest to 0,0,0
+func simulateParticles(particles []particle) (partsLeft int) {
+	checkCollisions(particles)
+	for i := 0; i < 500; i++ {
+		for i := range particles {
+			step(&particles[i])
+		}
+		checkCollisions(particles)
+	}
+	for i := range particles {
+		if !particles[i].col {
+			partsLeft++
+		}
+	}
+	return
+}
+func checkCollisions(particles []particle) {
+	for i := 0; i < len(particles); i++ {
+		if particles[i].col {
+			continue
+		} 
+		for j := 0; j < len(particles);j++ {
+			if particles[j].col {
+				continue
+			}
+			if i != j && colliding(particles[i],particles[j]) {
+				particles[i].col = true
+				particles[j].col = true
+			}
+		}
+	}
+}
+func colliding(a,b particle) (bool) {
+	if a.pos.x == b.pos.x && a.pos.y == b.pos.y && a.pos.z == b.pos.z {
+		return true
+	}
+	return false
+}
 func main() {
-	parseAccel()
-	//there are only 2 particles with the lowest acceleration (2), 
-	//so just manually compute their initial velocity to find particle that stays closest to origin.
+	particles := parseInput()
+	x := simulateParticles(particles)
+	fmt.Println(x)
 }
