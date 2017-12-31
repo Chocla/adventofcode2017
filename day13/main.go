4ec4c99fd8de6b58038f8053e56982cab4a62245
Chocla <chocla1@cock.li>
2017-12-30 16:22:45 -0800

    Day 13 Part 2

--- day13/main.go
+++ day13/main.go
@@ -13,42 +13,27 @@ const length = 85
 
 func main(){
 	firewall := parseInput()
-	severity := 0
-	v := make([]bool, length)
-	for i := 0; i < length; i++ {
-		if len(firewall[i]) > 0 && firewall[i][0] == 1 {
-			severity += (i * len(firewall[i]))
-		}
-		stepFirewall(firewall,v)
-	}
+	i := 0
+	//loop forever until an attempt with delay i gets through
+	for ; attemptRun(firewall,i); i++ {
 
-	fmt.Println(severity)
+	}
+	fmt.Println(i)
 }
-func stepFirewall(fw [][]int, v []bool) {
-	for i := range fw {
-		//empty column
-		if len(fw[i]) == 0{
-			continue 
+func attemptRun(fw [][]int, delay int) (bool) {
+	for i := delay; i < delay + len(fw); i++ {
+		//skip empty columns
+		if len(fw[i-delay]) == 0 {
+			continue
 		}
-		for j := range fw[i] {
-			if fw[i][j] == 1 {
-				if j == 0 {
-					v[i] = false
-				} else if j == (len(fw[i])-1) {
-					v[i] = true
-				}
-				if !v[i] {
-					
-					fw[i][j],fw[i][j+1] = 0,1
-					break
-				} else {
-					fw[i][j],fw[i][j-1] = 0,1
-					break
-				}
-			}
+		//scanners repeat mod 2*(len - 1), so if the operation == 0, packet got caught
+		if i % (2 * (len(fw[i-delay]) - 1 )) == 0 {
+			return true
 		}
 	}
+	return false
 }
+
 func parseInput() ([][]int){
 	firewall := make([][]int,length)
 
